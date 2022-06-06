e6649643c2b02dafa13e50a85dba826087cc13bd
Valmo <val_mo_jr@hotmail.com>
2022-06-06 01:06:53 -0300

    adicionado atualizador de estoque

--- main.cpp
+++ main.cpp
@@ -85,7 +85,7 @@ void item::setorder_id(int pd_i) {
     order_id = pd_i;
 };
 void item::setQuantidade(int qtd) {
- quantidade = qtd;
+    quantidade = qtd;
 };
 int item::getID() {
     return id;
@@ -163,10 +163,10 @@ int main () {
     while (option != 4)
     {
         system("cls");
-        cout << "====== MENU =====\n";
+        cout << "======== MENU =======\n";
         cout << "1. Cadastrar produto\n";
-        cout << "2. Verificar estoque\n";
-        cout << "3. Realizar order\n";
+        cout << "2. Estoque\n";
+        cout << "3. Realizar Pedido\n";
         cout << "4. Sair\n";
         cin >> option;
         system("cls");
@@ -177,16 +177,16 @@ int main () {
             cout << "Deseja cadastrar um novo produto? (s/n)\n";
             cin >> soun;
             if (soun == 's')
-            {
-                product_id_count++;
+            {   
                 system("cls");
                 cout << "== CADASTRO DE PRODUTO ==\n";
-                double newprice;
-                cout << "Digite o valor por unidade(use ponto em vez de virgula): ";
-                cin >> newprice;
                 string newname;
                 cout << "\nDigite o nome do produto: ";
                 getline(cin >> std::ws, newname);
+                product_id_count++;
+                double newprice;
+                cout << "Digite o valor por unidade(use ponto em vez de virgula): ";
+                cin >> newprice;
                 int newqtd;
                 cout << "\nDigite a quantidade em estoque: ";
                 cin >> newqtd;
@@ -199,8 +199,15 @@ int main () {
         };
         if (option == 2)
         {   
-            {
+            cout << "=====ESTOQUE=====\n";
+            cout << "1. Verificar Estoque\n";
+            cout << "2. Atualizar quantidade de produto\n";
+            cin >> option;
+            system("cls");
+            if (option == 1) {
+                // VERIFICAR ESTOQUE
                 cout << "=====ESTOQUE=====\n";
+                cout << "ID - NOME - VALOR - QTD\n";
                 for (int i = 1; i < product_id_count+1; i++) {
                     cout << products[i].getID() << ". " << products[i].getName() << " - " << products[i].getPrice()<< " - " << products[i].getEstoque() << "\n";
                 };
@@ -208,7 +215,21 @@ int main () {
                 cout << "\nContinuar? (s/n)\n";
                 cin >> soun;
                 system("cls");
-            };
+            }
+            else if (option == 2) {
+                // ATUALIZAR QTD DE ALGUM PRODUTO CADASTRADO
+                int prod_id;
+                cout << "=====ESTOQUE=====\n";
+                cout << "Digite o ID do produto: ";
+                cin >> prod_id;
+                cout << "Qual a quantidade de " << products[prod_id].getName() << " em estoque?\n";
+                int newqtd;
+                cin >> newqtd;
+                products[prod_id].setEstoque(newqtd);
+                system("cls");
+            } else {
+                cout << "Opcao incorreta!";
+            }
         }
 
         if (option == 3)
